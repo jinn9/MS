8f7a30a32ff2ccb7c0b543b325214623b7e1309e
Jin <jin.dev.1425@gmail.com>
2025-05-20 17:02:23 -0400

    implement eureka server

---
+++ eurekaserver/src/test/java/com/github/jinn9/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jinn9.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
