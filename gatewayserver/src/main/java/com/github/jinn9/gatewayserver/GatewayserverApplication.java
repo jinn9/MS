98933526a650e9fc8f7fce519b69ee998f4ff5bc
Jin <jin.dev.1425@gmail.com>
2025-05-25 23:41:28 -0400

    Comment out custom routing config

--- gatewayserver/src/main/java/com/github/jinn9/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/github/jinn9/gatewayserver/GatewayserverApplication.java
@@ -10,4 +10,26 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+
+//	Config for custom routing
+//	@Bean
+//	public RouteLocator routeConfig(RouteLocatorBuilder routeLocatorBuilder) {
+//		return routeLocatorBuilder.routes()
+//				.route(p -> p
+//						.path("/amazonna/member/**")
+//						.filters( f -> f.rewritePath("/amazonna/member/(?<segment>.*)","/${segment}")
+//								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+//						.uri("lb://MEMBER"))
+//				.route(p -> p
+//						.path("/amazonna/product/**")
+//						.filters( f -> f.rewritePath("/amazonna/product/(?<segment>.*)","/${segment}")
+//								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+//						.uri("lb://PRODUCT"))
+//				.route(p -> p
+//						.path("/amazonna/order/**")
+//						.filters( f -> f.rewritePath("/amazonna/order/(?<segment>.*)","/${segment}")
+//								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+//						.uri("lb://ORDER")).build();
+//	}
+
 }
