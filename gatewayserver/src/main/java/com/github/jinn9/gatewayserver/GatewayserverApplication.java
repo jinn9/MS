a5a270c4f266c04f301aba2702e7cf01a05b6c12
Jin <jin.dev.1425@gmail.com>
2025-05-26 18:13:28 -0400

    configure rate limiter in gatewayserver

--- gatewayserver/src/main/java/com/github/jinn9/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/github/jinn9/gatewayserver/GatewayserverApplication.java
@@ -1,11 +1,19 @@
 package com.github.jinn9.gatewayserver;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -29,7 +37,9 @@ public class GatewayserverApplication {
 								.circuitBreaker(config -> config.setName("memberCircuitBreaker"))
 								.retry(retryConfig -> retryConfig.setRetries(3)
 										.setMethods(HttpMethod.GET)
-										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
+										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
+								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+										.setKeyResolver(userKeyResolver())))
 						.uri("lb://MEMBER"))
 				.route(p -> p
 						.path("/product/**")
@@ -38,7 +48,9 @@ public class GatewayserverApplication {
 								.circuitBreaker(config -> config.setName("productCircuitBreaker"))
 								.retry(retryConfig -> retryConfig.setRetries(3)
 										.setMethods(HttpMethod.GET)
-										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
+										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
+								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+										.setKeyResolver(userKeyResolver())))
 						.uri("lb://PRODUCT"))
 				.route(p -> p
 						.path("/order/**")
@@ -47,8 +59,28 @@ public class GatewayserverApplication {
 								.circuitBreaker(config -> config.setName("orderCircuitBreaker"))
 								.retry(retryConfig -> retryConfig.setRetries(3)
 										.setMethods(HttpMethod.GET)
-										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
+										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
+								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+										.setKeyResolver(userKeyResolver())))
 						.uri("lb://ORDER")).build();
+	}
+
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
+	}
+
+	@Bean
+	public RedisRateLimiter redisRateLimiter() {
+		return new RedisRateLimiter(1, 1, 1);
+	}
+
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+				.defaultIfEmpty("anonymous");
 	}
 
 }
