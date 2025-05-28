9442b78e17468bc8079697cc43e1ff6cdaa249d4
Jin <jin.dev.1425@gmail.com>
2025-05-28 18:11:30 -0400

    implement authorization using roles

--- gatewayserver/src/main/java/com/github/jinn9/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/github/jinn9/gatewayserver/config/SecurityConfig.java
@@ -2,11 +2,16 @@ package com.github.jinn9.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -16,14 +21,23 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         // turn gateway server into a resource server
         serverHttpSecurity.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.pathMatchers(HttpMethod.GET).permitAll()
-                        .pathMatchers("/order/**").authenticated()
-                        .pathMatchers("/member/**").authenticated()
-                        .pathMatchers("/product/**").authenticated())
-                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
+                        .pathMatchers("/order/**").hasRole("ORDER")
+                        .pathMatchers("/member/**").hasRole("MEMBER")
+                        .pathMatchers("/product/**").hasRole("PRODUCT"))
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(jwtSpec ->
+                        jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
 
         // disable csrf
         serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
 
         return serverHttpSecurity.build();
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter =
+                new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter
+                (new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 }
